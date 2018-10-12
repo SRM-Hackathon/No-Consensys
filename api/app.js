28265e380321c0d3203955ed7d959901b5f4aab4
sriram <sriramklash@gmail.com>
2018-10-13 01:59:17 +0530

    Connect to database

--- api/app.js
+++ api/app.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const pg = require('pg');
+
+require('dotenv').config();
 
 const app = express();
+
+const pgClient = new pg.Client({
+	host: process.env.DB_HOST,
+	port: Number(process.env.DB_PORT),
+	database: process.env.DB_NAME,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	keepAlive: true
+});
+pgClient.connect()
+	.then(() => {
+		console.log('Connected to database');
+	})
+	.catch((err) => {
+		console.error('Error connecting to database: ' + err);
+	});
 
 const ApiRouter = require('./routers/apiRouter');
 
@@ -10,3 +29,7 @@ const port = 3000;
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
 });
+
+module.exports = {
+	PgClient: pgClient
+}
