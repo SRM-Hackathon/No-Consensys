1b1d6858064d2bc7b07a198d66088d57350853c9
sriram <sriramklash@gmail.com>
2018-10-13 03:02:12 +0530

    Handle post requests for creating NGOs

--- api/controllers/ngoController.js
+++ api/controllers/ngoController.js
@@ -17,3 +17,33 @@ module.exports.listAllNgos = function(req, res) {
 			});
 		});
 }
+
+module.exports.createNgo = function(req, res) {
+	if (req.get('Content-Type') != 'application/json') {
+		res.status(400);
+		res.json({
+			error: 'Content type should be application/json'
+		});
+		return;
+	}
+	
+	let name = req.body.name;
+	let description = req.body.description;
+	let address = req.body.address;
+
+	const query = `INSERT INTO ngos VALUES (default, $1, $2, $3)`;
+
+	PgClient.query(query, [name, description, address])
+		.then(() => {
+			res.status(201);
+			res.json({
+				message: 'NGO has been created successfully'
+			});
+		})
+		.catch((err) => {
+			res.status(500);
+			res.json({
+				error: 'Could not create NGO'
+			});
+		});
+}
--- api/routers/ngoRouter.js
+++ api/routers/ngoRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const NgoController = require('../controllers/ngoController');
 
 router.get('/', NgoController.listAllNgos);
+router.post('/', NgoController.createNgo);
 
 module.exports = router;
