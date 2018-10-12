06475e3d10e039b120bbbde1da3b160e04c9d834
sriram <sriramklash@gmail.com>
2018-10-13 00:49:34 +0530

    Add api router

--- api/app.js
+++ api/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const ApiRouter = require('./routers/apiRouter');
+
+app.use('/api/', ApiRouter);
+
 const port = 3000;
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
