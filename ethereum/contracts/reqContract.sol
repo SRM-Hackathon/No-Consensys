196474238b4c3dc99a8638386430e0b3b47a3467
Rahul Suresh <rahul.tech.work@gmail.com>
2018-10-12 17:54:22 +0530

    Update ethereum/contracts/reqContract.sol

--- ethereum/contracts/reqContract.sol
+++ ethereum/contracts/reqContract.sol
@@ -16,7 +16,7 @@ contract NewRequest {
     
     struct Request{
         string description;
-        uint minVal;
+        uint monReq;
         address Merchant;
         bool complete;
         uint appCount;
@@ -25,7 +25,7 @@ contract NewRequest {
     
     Request[] requests;
     
-   constructor (uint totalCon)
+    constructor (uint totalCon) public
     {
         totalContribution=totalCon;
         minimumContribution=uint(totalCon/20);
@@ -40,48 +40,15 @@ contract NewRequest {
         
         contriRecieved=this.balance;
     }
-    
-pragma solidity ^0.4.21;
-
-
-
-contract NewRequest {
-    
-    address public manager;
-    uint public totalContribution;
-    uint public contriRecieved;
-    uint public minimumContribution;
-    mapping(address=>bool) donors;
-    
-    uint public donationRecived;
-    
-    uint donorCount;
-    
-    struct Request{
-        string description;
-        uint minVal;
-        address Merchant;
-        bool complete;
-        uint appCount;
-        mapping(address=>bool) approved;
-    }
-    
-    Request[] requests;
-    
-   constructor (uint totalCon)
+    function createRequest(string description,uint monReq, address Merchant)
     {
-        totalContribution=totalCon;
-        minimumContribution=uint(totalCon/20);
-        manager=msg.sender;
-        contriRecieved=0;
-    }
-    
-    function donateMoney () payable public
-    {
-        require(msg.value> minimumContribution);
-        donors[msg.sender]=true;
-        // to save total value remaining in contract
-        contriRecieved=this.balance;
+        Request memory newReq= Request({
+            description:description,
+            monReq:monReq,
+            Merchant:Merchant,
+            appCount:0,
+            complete:false
+        });
     }
     
 }
