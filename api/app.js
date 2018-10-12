670ee5d6dbc9dc7a63378c5a97f5b3eaf8fc280b
sriram <sriramklash@gmail.com>
2018-10-13 02:39:10 +0530

    Use body parser middleware

--- api/app.js
+++ api/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const pg = require('pg');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const pgClient = new pg.Client({
 	host: process.env.DB_HOST,
