ebf9b54ed507c0ce75f59ca646225e68821117eb
Rahul Suresh <rahul.tech.work@gmail.com>
2018-10-13 14:36:12 +0530

    ngo integrating with backend

--- api/app.js
+++ api/app.js
@@ -32,7 +32,7 @@ const ApiRouter = require('./routers/apiRouter');
 
 app.use('/api/', ApiRouter);
 
-const port = 8011;
+const port = 8012;
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
 });
